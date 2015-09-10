12d5d9d03f1c305235139592620c465fdaa1e0b2
Eric Euresti <eeuresti@gmail.com>
2015-09-10 15:48:34 -0700

    error codes on signup working

--- index.js
+++ index.js
@@ -66,26 +66,41 @@ app.get("/signup", function (req, res) {
 
 // where the user submits the sign-up form
 app.post(["/users", "/signup"], function signup(req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-
-
-  console.log(email + " has signed up");
-  db.User.createSecure(email, password, function(err, user) {
-    req.login(user);
-    res.redirect("/login");
-  });
+  if (req.body.password.length < 6) {
+  		res.status(400).send({message: "invalid password: Needs to be at least 8 characters"});
+  	} else {
+  		db.User.find({email: req.body.email}, function(err, found) {
+  			if (found.length > 0) {
+  				res.status(400).send({message: "email already exists"});
+  			} else {
+          var newUser = {
+            email: req.body.email,
+            password: req.body.password
+          };
+          db.User.createSecure(newUser.email, newUser.password, function(err, user) {
+            req.login(user);
+                res.redirect("/login");
+          });
+			}
+		});
+	}
 });
 
+
 // where the user submits the login form
-app.post(["/sessions", "/login"], function login(req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-    console.log(email + " has logged in");
-  db.User.authenticate(email, password, function (err, user) {
-    req.login(user);
-    res.redirect("/login");
-  });
+app.post("/login", function(req, res) {
+	var userData = {
+		email: req.body.email,
+		password: req.body.password
+	};
+	db.User.authenticate(userData.email, userData.password, function(err, user) {
+		if (user !== undefined) {
+			req.login(user);
+			res.redirect("/login");
+		} else {
+			res.status(400).send({message: "bad username or password"});
+		}
+	});
 });
 
 // show the current user
@@ -105,6 +120,6 @@ app.delete(["/sessions", "/logout"], function(req, res) {
   res.redirect("/");
 });
 
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + listener.address().port);
 });
